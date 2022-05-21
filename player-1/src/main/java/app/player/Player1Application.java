18afd67828497ef112febdd096946bda3bdcf362
= <=>
2022-05-21 20:15:43 +0600

    initial commit

---
+++ player-1/src/main/java/app/player/Player1Application.java
@@ -0,0 +1,12 @@
+package app.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Player1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Player1Application.class, args);
+    }
+}
---
+++ player-1/src/test/java/app/player/player1/Player1ApplicationTests.java
@@ -0,0 +1,13 @@
+package app.player.player1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Player1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
