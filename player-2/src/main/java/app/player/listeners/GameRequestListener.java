2c09f8532dd1167d6870676661654b577a51dd8c
= <=>
2022-05-22 15:27:03 +0600

    Added log

--- player-2/src/main/java/app/player/listeners/GameRequestListener.java
+++ player-2/src/main/java/app/player/listeners/GameRequestListener.java
@@ -1,5 +1,6 @@
 package app.player.listeners;
 
+import app.player.events.InitQueueEvent;
 import app.player.game.GameInitRequest;
 import com.rabbitmq.client.Channel;
 import java.io.IOException;
@@ -7,6 +8,7 @@ import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -15,10 +17,16 @@ import org.springframework.stereotype.Component;
 public class GameRequestListener {
 
   private final Channel channel;
+  private final ApplicationEventPublisher applicationEventPublisher;
 
   @RabbitListener(queues = "game-of-three")
-  void listenGameRequest(GameInitRequest request) throws IOException, InterruptedException {
+  void listenGameRequest(GameInitRequest request) throws IOException {
+    log.debug(
+        "Received handshake request, incoming: {} and outgoing: {}",
+        request.incomingQueue(),
+        request.outgoingQueue());
     channel.queueDeclare(request.incomingQueue(), false, false, true, Collections.emptyMap());
     channel.queueDeclare(request.outgoingQueue(), false, false, true, Collections.emptyMap());
+    applicationEventPublisher.publishEvent(new InitQueueEvent(request));
   }
 }
