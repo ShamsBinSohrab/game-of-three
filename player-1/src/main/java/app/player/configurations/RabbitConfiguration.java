cba7ed96efc3fea85cc1e1970c145c85b0d25530
= <=>
2022-05-25 16:02:29 +0600

    Removed rabbit admin bean

--- player-1/src/main/java/app/player/configurations/RabbitConfiguration.java
+++ player-1/src/main/java/app/player/configurations/RabbitConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,11 +28,6 @@ public class RabbitConfiguration {
   @Bean
   public Binding binding(Queue queue, DirectExchange exchange) {
     return BindingBuilder.bind(queue).to(exchange).withQueueName();
-  }
-
-  @Bean
-  public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-    return new RabbitAdmin(connectionFactory);
   }
 
   @Bean
