8e256270828e7ece0d5d43d6e41af25672b593c3
= <=>
2022-05-25 17:02:43 +0600

    refactored player 2 architecture

--- player-2/src/main/java/app/player/events/EventFactory.java
+++ player-2/src/main/java/app/player/events/EventFactory.java
@@ -1,5 +1,7 @@
 package app.player.events;
 
+import app.player.domains.Move;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
@@ -9,4 +11,12 @@ import org.springframework.stereotype.Component;
 public class EventFactory {
 
   private final ApplicationEventPublisher applicationEventPublisher;
+
+  public void logSentMove(Move move, UUID correlationId) {
+    applicationEventPublisher.publishEvent(new LogSentMoveEvent(move, correlationId));
+  }
+
+  public void logReceivedMove(Move move, UUID correlationId) {
+    applicationEventPublisher.publishEvent(new LogReceivedMoveEvent(move, correlationId));
+  }
 }
--- player-2/src/main/java/app/player/listeners/GameRequestListener.java
+++ player-2/src/main/java/app/player/listeners/GameRequestListener.java
@@ -1,6 +1,7 @@
 package app.player.listeners;
 
 import app.player.domains.Move;
+import app.player.events.EventFactory;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class GameRequestListener {
 
+  private final EventFactory eventFactory;
   private final RabbitTemplate rabbitTemplate;
 
   @RabbitListener(queues = "game-queue", concurrency = "5")
@@ -22,28 +24,27 @@ public class GameRequestListener {
       log.info("Opponent won game: {}", move.gameId());
       return;
     }
-    log.debug(
-        "Received message: {}, move: {}", message.getMessageProperties().getCorrelationId(), move);
+    var receivedCorrelationId = UUID.fromString(message.getMessageProperties().getCorrelationId());
+    eventFactory.logReceivedMove(move, receivedCorrelationId);
+
     var nextMove = move.nextMove();
-    var correlationId = UUID.randomUUID().toString();
+    var correlationId = UUID.randomUUID();
     if (nextMove.didIWin()) {
-      rabbitTemplate.convertAndSend(
-          message.getMessageProperties().getReplyTo(),
-          move.checkmate(),
-          msg -> {
-            msg.getMessageProperties().setCorrelationId(correlationId);
-            return msg;
-          });
-      log.debug("I won");
+      log.info("I won game: {}", nextMove.gameId());
+      send(message.getMessageProperties().getReplyTo(), nextMove.checkmate(), correlationId);
       return;
     }
-    log.debug("Sending message: {}, move: {}", correlationId, nextMove);
+    send(message.getMessageProperties().getReplyTo(), nextMove, correlationId);
+    eventFactory.logSentMove(nextMove, correlationId);
+  }
+
+  private void send(String replyQueue, Move move, UUID correlationId) {
     rabbitTemplate.convertAndSend(
-        message.getMessageProperties().getReplyTo(),
-        nextMove,
-        msg -> {
-          msg.getMessageProperties().setCorrelationId(correlationId);
-          return msg;
+        replyQueue,
+        move,
+        message -> {
+          message.getMessageProperties().setCorrelationId(correlationId.toString());
+          return message;
         });
   }
 }
