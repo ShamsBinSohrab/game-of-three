789d66b73485b78852ae331fb39f37988afeac4b
= <=>
2022-05-29 09:04:46 +0600

    Fixed arm64 build issue

--- build_local_images.sh
+++ build_local_images.sh
@@ -2,13 +2,13 @@
 
 echo "Building image for player 1"
   case "$(uname -p)" in
-   arm) ./mvnw clean compile jib:dockerBuild -- -Djib-maven-plugin.architecture=arm64 -f player-1/pom.xml ;;
+   arm) ./mvnw clean compile jib:dockerBuild -Djib-maven-plugin.architecture=arm64 -f player-1/pom.xml ;;
    *) ./mvnw clean compile jib:dockerBuild -f player-1/pom.xml ;;
   esac
 
 echo "Building image for player 2"
   case "$(uname -p)" in
-   arm) ./mvnw clean compile jib:dockerBuild -- -Djib-maven-plugin.architecture=arm64 -f player-2/pom.xml ;;
+   arm) ./mvnw clean compile jib:dockerBuild -Djib-maven-plugin.architecture=arm64 -f player-2/pom.xml ;;
    *) ./mvnw clean compile jib:dockerBuild -f player-2/pom.xml ;;
   esac
 
